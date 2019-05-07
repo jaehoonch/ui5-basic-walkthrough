839504bb6f8b3ba70c4edf7a1fe78848a67fb6e5
jaehoonch <jaehoonch@hotmail.com>
2019-05-08 01:45:05 +0900

    Created initial Fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,30 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller){
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment){
 	"use strict";
-	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+		onShowHello: function(){
+			
+		},
+		onOpenDialog: function(){
+			var oView = this.getView();
+			
+			// create dialog lazily
+			if(!this.byId("helloDialog")){
+				// load asynchronous XMl fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap/ui.demo.walkthrough.view.HelloDialog"
+				}).then(function (oDialog){
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else{
+				this.byId("helloDialog").open();
+			}
+		}
 	});
 });
