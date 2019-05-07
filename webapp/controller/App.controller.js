8107d1777ae1f6eaf8c9f6e3ce052497431862d0
jaehoonch <jaehoonch@hotmail.com>
2019-05-07 23:41:47 +0900

    Added JSON Model and Resource Model (i18n)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,32 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller, MessageToast){
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function(Controller, MessageToast, JSONModel, ResourceModel){
 	"use strict";
-	return Controller.extend("", {
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+		onInit: function(){
+			// set data model on view
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+			// set i18n model on view
+			var i18nMode = new ResourceModel({
+				bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+			});
+			this.getView().setModel(i18nMode, "i18n");
+		},
 		onShowHello: function(){
-			MessageToast.show("Hello World");
+			// read message from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			MessageToast.show(sMsg);
 		}
 	});
 });
